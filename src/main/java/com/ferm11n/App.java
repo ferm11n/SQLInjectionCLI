d9b2034115984139698bc9079f71c7ac0119c5de
Lucas <lucasferminnieto@yahoo.com.ar>
2024-11-16 15:26:27 -0300

    init proyect

---
+++ src/main/java/com/ferm11n/App.java
@@ -0,0 +1,13 @@
+package com.ferm11n;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
